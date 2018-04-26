54c271bcc003a2aa6e8808b83e2eb10a60cf2b7a
Muhammad Yusuf <mail.secondson@gmail.com>
2018-04-26 13:22:58 +0700

    Create Project

---
+++ src/main/java/com/secondson/perpus/PerpusApplication.java
@@ -0,0 +1,12 @@
+package com.secondson.perpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpusApplication.class, args);
+	}
+}
